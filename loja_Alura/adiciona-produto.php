5547b7440a5d2287b65531c96c1fc8548e5e7e4d
Willian Ohara <wyohara@gmail.com>
2017-03-10 13:03:40 -0300

    Aula 2 - salvando dados no mysql

--- loja_Alura/adiciona-produto.php
+++ loja_Alura/adiciona-produto.php
@@ -1,9 +1,28 @@
 <?php include("cabecalho.php");?>
 	<h1>Bem Vindo</h1>
 	<?php
+		//recuperando valores do produto-form
 		$nome = $_GET["nome"];
 		$preco = $_GET["preco"];
+
+		//criando a conexao com o banco de dados
+		$conexao = mysqli_connect("localhost","root", "", "Loja");
+
+		//Criando a query para inserir no banco
+		$query = "INSERT INTO produtos(nome, preco) VALUES ('{$nome}', '{$preco}')";
+
+		$resultado = mysqli_query($conexao, $query); //realizando a query
+
+		if ($resultado){
+			//aviso de sucesso na query?>
+			<p class="alert-success">Produto <?php echo $nome;?> adicionado.Seu valor é <?php echo $preco?>.</p>
+			<?php
+		}else{
+			//aviso de erro na query?>
+			<p class="alert-danger">O Produto  não foi adicionado</p>
+			<?php
+		}
+		mysqli_close($conexao);//encerrando a conexao
 	?>
-	<p class="alert-success">Produto <?php echo $nome;?> adicionado.
-	Seu valor é <?php echo $preco?>.</p>
+
 <?php include("rodape.php");?>
